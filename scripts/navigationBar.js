b7083123959e97b0d34ef8d869b3b6c65a8cae58
Rubén Naranjo <rnaranjo@gmail.com>
2017-07-31 21:07:44 +0100

    Initial commit

---
+++ scripts/navigationBar.js
@@ -0,0 +1,8 @@
+function responsiveNavBar() {
+    var x = document.getElementById("topNavigationBar");
+    if (x.className === "navigationBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navigationBar";
+    }
+}
