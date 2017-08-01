11574b61ceea5527639b86c84247ff48570d5718
Rubén Naranjo <rnaranjo@gmail.com>
2017-08-01 22:21:50 +0100

    Added hover animation

--- scripts/navigationBar.js
+++ scripts/navigationBar.js
@@ -1,7 +1,7 @@
 function responsiveNavBar() {
     var x = document.getElementById("topNavigationBar");
     if (x.className === "navigationBar") {
-        x.className += " responsive";
+        x.className += "-responsive";
     } else {
         x.className = "navigationBar";
     }
