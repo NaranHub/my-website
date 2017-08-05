8fe3377d0767891716d72a71fc180d8cd8cf45e2
Rubén Naranjo <rnaranjo@gmail.com>
2017-08-05 14:28:02 +0100

    Responsive for small devices
    Fixed menu
    Fixed togle bar
    Added animations
    Aligned menu items

--- scripts/navigationBar.js
+++ scripts/navigationBar.js
@@ -1,7 +1,7 @@
 function responsiveNavBar() {
     var x = document.getElementById("topNavigationBar");
     if (x.className === "navigationBar") {
-        x.className += "-responsive";
+        x.className += " dropdown";
     } else {
         x.className = "navigationBar";
     }
