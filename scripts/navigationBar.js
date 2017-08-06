8a19ecfb1f252f2c4950729e872e79106cf296f4
Rubén Naranjo <rnaranjo@gmail.com>
2017-08-06 22:21:47 +0100

    Menu closes when clicking outside

--- scripts/navigationBar.js
+++ scripts/navigationBar.js
@@ -1,8 +1,28 @@
+// Sets up the navigation bar on window load 
+window.onload = function() {
+    // Adds a listener to prevent the dropdown closer to close the dropdown right after opening it
+    var nb = document.getElementById("topNavigationBar");
+    nb.addEventListener('click', function(e) {e.stopPropagation()}, false);
+}
+
+// Deploys and collapses the dropdown menu in small devices
 function nbToggle() {
     var nb = document.getElementById("topNavigationBar");
     if (nb.className === "navigationBar") {
+        document.body.addEventListener('click', dropdownCloser, false);
         nb.classList.add('dropdown');
-    } else {
+    } 
+    else {
+        document.body.removeEventListener('click', dropdownCloser, false);
         nb.classList.remove('dropdown');
     }
 }
+
+// Closes the dropdown menu when open and clicked outside it
+function dropdownCloser(e) {
+    if(e.target.id != 'topNavigationBar'){
+        document.body.removeEventListener('click', dropdownCloser, false);
+        var nb = document.getElementById("topNavigationBar");
+        nb.classList.remove('dropdown');
+    }
+}   
