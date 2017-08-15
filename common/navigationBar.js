1efa740faf330faff4a141af0ffb66c5881934be
Rubén Naranjo <rnaranjo@gmail.com>
2017-08-15 20:33:03 +0100

    Fixed delayed navigation

--- common/navigationBar.js
+++ common/navigationBar.js
@@ -1,26 +1,28 @@
 // Sets up the navigation bar (to be called once the HTML is loaded)
 function initNavigationBar() {
     // Adds a listener to prevent the dropdown closer to close the dropdown right after opening it
-    var nb = document.getElementById("topNavigationBar");
+    var nb = document.getElementById('topNavigationBar');
     addListenerToEvents(nb, 'click touchstart', e => e.stopPropagation());
     
     // Detect if current page is part of the menu bar and highlight it
     var url = window.location.pathname;
-    var links = document.getElementById("nb-menu-items").getElementsByClassName("nb-menu-link");
+    var links = document.getElementById('nb-menu-items').getElementsByClassName('nb-menu-link');
     var i, href;
-    for (i=0; i<links.length; i++) {
-        href = links[i].getAttribute('href');
-        if(href.includes(url)) {
-            links[i].classList.add('nb-menu-link-current');
-        }       
-    }
+    if (url.length > 1) { // Avoid '/' case
+        for (i=0; i<links.length; i++) {
+            href = links[i].getAttribute('href');
+            if(href.includes(url)) {
+                links[i].classList.add('nb-menu-link-current');
+            }       
+        }
+    } 
 }
 
 // Deploys and collapses the dropdown menu in small devices
 function nbToggle() {
     if(isToggleNavVisible()) {
-        var nb = document.getElementById("topNavigationBar");
-        if (nb.className === "navigationBar") {
+        var nb = document.getElementById('topNavigationBar');
+        if (nb.className === 'navigationBar') {
             addListenerToEvents(document.body, 'click touchstart', dropdownCloser);
             nb.classList.add('dropdown');
         } 
@@ -35,14 +37,14 @@ function nbToggle() {
 function dropdownCloser(e) {
     if(e.target.id != 'topNavigationBar'){
         removeListenerFromEvents(document.body, 'click touchstart', dropdownCloser);
-        var nb = document.getElementById("topNavigationBar");
+        var nb = document.getElementById('topNavigationBar');
         nb.classList.remove('dropdown');
     }
 }
 
 // Delays the redirection to an URL
 // Used in order to allow animations to finish before redirecting
-function delayRedirection(URL) {
+function delayedRedirection(URL) {
     if(isToggleNavVisible()) {
         setTimeout(() => { window.location = URL; }, 500);
     }
@@ -53,6 +55,6 @@ function delayRedirection(URL) {
 
 // Checks if the ToggleNav is visible (small display) or not (big display)
 function isToggleNavVisible() {
-    var tn = document.getElementById("toggle-nav");
+    var tn = document.getElementById('toggle-nav');
     return isVisible(tn);    
 }
